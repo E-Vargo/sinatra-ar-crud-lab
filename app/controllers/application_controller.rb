ee0cf319057032f018ba37f9ed7ff318613fd913
E-Vargo <vargoelliott@gmail.com>
2022-01-13 12:54:58 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/show' do 
+    erb :show
+  end
+
+  get '/index' do
+    erb :index 
+  end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do 
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index 
+  end 
+
+  delete '/articles/:id' do
+    Article.find(params[:id]).delete
+    erb :show
+    redirect to "/articles"
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
 end
